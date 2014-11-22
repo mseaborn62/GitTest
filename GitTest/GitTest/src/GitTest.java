467c31f0ba0584735f8de03030db31ebb393e750
Mark Seaborn <mseaborn62@gmail.com>
2014-11-22 17:38:11 -0600

    added test 1

--- GitTest/GitTest/src/GitTest.java
+++ GitTest/GitTest/src/GitTest.java
@@ -2,6 +2,12 @@
 public class GitTest {
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		test1();
 	}
-
+	
+	public static void test1(){
+		for (int i=0; i<19; i++){
+			System.out.println("Test 1 " + i);
+		}
+	}
 }
