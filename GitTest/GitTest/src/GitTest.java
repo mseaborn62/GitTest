eb665b66d11aa828e04ccdefe67e213b78d1f97b
Mark Seaborn <mseaborn62@gmail.com>
2014-11-22 19:36:16 -0600

    Added test 2

--- GitTest/GitTest/src/GitTest.java
+++ GitTest/GitTest/src/GitTest.java
@@ -3,11 +3,19 @@ public class GitTest {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		test1();
+		test2();
+		test3();
 	}
 	
 	public static void test1(){
 		for (int i=0; i<19; i++){
 			System.out.println("Test 1 " + i);
+		}
+	}
+	
+	public static void test2(){
+		for(int i=0; i<2; i++){
+			System.out.println("Test 2 " + i);
 		}
 	}
 	
